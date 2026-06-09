529e68684911855d6e7c594094452aff4ecc1ec3
Supat T <supat.t@socket9.com>
2026-06-09 09:58:31 +0700

    chore: ignore ts and eslint during build to prevent oom

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   serverExternalPackages: ["@prisma/client", "prisma"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
